82a6aa70698dce57920126732eed3bc470f10103
joeycool99 <naveenjoe@gmail.com>
2021-10-08 11:54:19 +0530

    port

--- index.js
+++ index.js
@@ -1,94 +1,93 @@
-
-
-
 import express from "express"
 import { Collection, MongoClient } from "mongodb"
 const app = express()
 import dotenv from "dotenv"
 
+const PORT = process.env.PORT
+
 dotenv.config()
-// const users = [
-//     {
-//         "createdAt": "2021-10-01T00:49:47.780Z",
-//         "name": "Bennie Aufderhar",
-//         "avatar": "https://cdn.fakercloud.com/avatars/d_kobelyatsky_128.jpg",
-//         "age": 59,
-//         "color": "silver",
-//         "id": "5"
-//     },
-//     {
-//         "createdAt": "2021-09-30T14:22:51.638Z",
-//         "name": "Lana Witting",
-//         "avatar": "https://cdn.fakercloud.com/avatars/afusinatto_128.jpg",
-//         "age": 77,
-//         "color": "olive",
-//         "id": "6"
-//     },
-//     {
-//         "createdAt": "2021-09-30T18:01:06.642Z",
-//         "name": "Vickie Brekke",
-//         "avatar": "https://cdn.fakercloud.com/avatars/carlyson_128.jpg",
-//         "age": 80,
-//         "color": "tan",
-//         "id": "7"
-//     },
-//     {
-//         "createdAt": "2021-09-30T09:39:22.586Z",
-//         "name": "Al Runolfsdottir",
-//         "avatar": "https://cdn.fakercloud.com/avatars/areus_128.jpg",
-//         "age": 28,
-//         "color": "orange",
-//         "id": "8"
-//     },
-//     {
-//         "createdAt": "2021-09-30T18:22:41.955Z",
-//         "name": "Sam Orn",
-//         "avatar": "https://cdn.fakercloud.com/avatars/itolmach_128.jpg",
-//         "age": 49,
-//         "color": "indigo",
-//         "id": "9"
-//     },
-//     {
-//         "createdAt": "2021-09-30T18:30:05.224Z",
-//         "name": "Grace Grimes",
-//         "avatar": "https://cdn.fakercloud.com/avatars/smalonso_128.jpg",
-//         "age": 72,
-//         "color": "yellow",
-//         "id": "10"
-//     },
-//     {
-//         "createdAt": "2021-09-30T11:26:57.667Z",
-//         "name": "Cindy Reinger",
-//         "avatar": "https://cdn.fakercloud.com/avatars/vimarethomas_128.jpg",
-//         "age": 30,
-//         "color": "yellow",
-//         "id": "11"
-//     },
-//     {
-//         "createdAt": "2021-10-01T06:26:55.203Z",
-//         "name": "Beth Koelpin",
-//         "avatar": "https://cdn.fakercloud.com/avatars/anatolinicolae_128.jpg",
-//         "age": 0,
-//         "color": "purple",
-//         "id": "12"
-//     },
-//     {
-//         "createdAt": "2021-09-30T12:28:17.426Z",
-//         "name": "Doug Mayer",
-//         "avatar": "https://cdn.fakercloud.com/avatars/nerrsoft_128.jpg",
-//         "age": 25,
-//         "color": "cyan",
-//         "id": "13"
-//     },
-//     {
-//         "createdAt": "2021-10-01T01:09:41.654Z",
-//         "name": "Mrs. Garrett Becker",
-//         "avatar": "https://cdn.fakercloud.com/avatars/increase_128.jpg",
-//         "age": 20,
-//         "color": "yellow",
-//         "id": "14"
-//     }
-// ]
+const users = [
+    {
+        "createdAt": "2021-10-01T00:49:47.780Z",
+        "name": "Bennie Aufderhar",
+        "avatar": "https://cdn.fakercloud.com/avatars/d_kobelyatsky_128.jpg",
+        "age": 59,
+        "color": "silver",
+        "id": "5"
+    },
+    {
+        "createdAt": "2021-09-30T14:22:51.638Z",
+        "name": "Lana Witting",
+        "avatar": "https://cdn.fakercloud.com/avatars/afusinatto_128.jpg",
+        "age": 77,
+        "color": "olive",
+        "id": "6"
+    },
+    {
+        "createdAt": "2021-09-30T18:01:06.642Z",
+        "name": "Vickie Brekke",
+        "avatar": "https://cdn.fakercloud.com/avatars/carlyson_128.jpg",
+        "age": 80,
+        "color": "tan",
+        "id": "7"
+    },
+    {
+        "createdAt": "2021-09-30T09:39:22.586Z",
+        "name": "Al Runolfsdottir",
+        "avatar": "https://cdn.fakercloud.com/avatars/areus_128.jpg",
+        "age": 28,
+        "color": "orange",
+        "id": "8"
+    },
+    {
+        "createdAt": "2021-09-30T18:22:41.955Z",
+        "name": "Sam Orn",
+        "avatar": "https://cdn.fakercloud.com/avatars/itolmach_128.jpg",
+        "age": 49,
+        "color": "indigo",
+        "id": "9"
+    },
+    {
+        "createdAt": "2021-09-30T18:30:05.224Z",
+        "name": "Grace Grimes",
+        "avatar": "https://cdn.fakercloud.com/avatars/smalonso_128.jpg",
+        "age": 72,
+        "color": "yellow",
+        "id": "10"
+    },
+    {
+        "createdAt": "2021-09-30T11:26:57.667Z",
+        "name": "Cindy Reinger",
+        "avatar": "https://cdn.fakercloud.com/avatars/vimarethomas_128.jpg",
+        "age": 30,
+        "color": "yellow",
+        "id": "11"
+    },
+    {
+        "createdAt": "2021-10-01T06:26:55.203Z",
+        "name": "Beth Koelpin",
+        "avatar": "https://cdn.fakercloud.com/avatars/anatolinicolae_128.jpg",
+        "age": 0,
+        "color": "purple",
+        "id": "12"
+    },
+    {
+        "createdAt": "2021-09-30T12:28:17.426Z",
+        "name": "Doug Mayer",
+        "avatar": "https://cdn.fakercloud.com/avatars/nerrsoft_128.jpg",
+        "age": 25,
+        "color": "cyan",
+        "id": "13"
+    },
+    {
+        "createdAt": "2021-10-01T01:09:41.654Z",
+        "name": "Mrs. Garrett Becker",
+        "avatar": "https://cdn.fakercloud.com/avatars/increase_128.jpg",
+        "age": 20,
+        "color": "yellow",
+        "id": "14"
+    }
+]
 
 async function createconnection() {
     // const MONGO_URL = "mongodb://localhost/users"
@@ -107,6 +106,11 @@ async function createconnection() {
 createconnection()
 
 app.use(express.json())
+
+
+
+
+
 app.get('/users', async function (req, res) {
     var client = await createconnection();
 
@@ -122,27 +126,20 @@ app.get('/users', async function (req, res) {
         res.send(user)
         // res.send(users.filter(data => data.color === (color)))
     }
-    if (agegt) {
+    else if (agegt) {
         const user = await client.db("users").collection("people").find({ age: { $gt: parseInt(agegt) } }).toArray()
         res.send(user)
     }
     else {
         const user = await client.db("users").collection("people").find({}).toArray()
         res.send(user)
+
     }
 })
 
-app.delete("users/:id", async (req, res) => {
-    console.log(req.params)
-    const { id } = req.params
-    const client = await createconnection();
-    const user = await client.db("users").collection("people").deleteOne({ id: id });
-    console.log(user)
-    res.send(user)
-})
 
-app.patch("users/:id", async (req, res) => {
-    console.log(req.params)
+app.patch("/users/:id", async (req, res) => {
+    console.log(req.body)
     const { id } = req.params
     const client = await createconnection()
     const newdata = req.body;
@@ -152,14 +149,25 @@ app.patch("users/:id", async (req, res) => {
     res.send(user)
 })
 
+app.delete("/users/:id", async (req, res) => {
+    console.log(req.params)
+    const { id } = req.params;
+
+    var client = await createconnection();
+    const user = await client.db("users").collection("people").deleteOne({ id: id })
+    console.log(user)
+
+    res.send(user)
+})
+
 app.post("/users", async (req, res) => {
     const client = await createconnection();
     console.log(req.body)
-    // var data = req.body
-    // const user = await client.db("users").collection("people").insertMany(data);
-    // res.send(user)
+    var data = req.body
+    const user = await client.db("users").collection("people").insertMany(data);
+    res.send(user)
 
-})
+});
 
 app.get("/users/:id", async (req, res) => {
     var { q } = req.query;
